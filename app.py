8a5864715351d3c2be8e680297f0dad72e3dd54a
srbism2 <162742149+srbism2@users.noreply.github.com>
2024-03-09 00:43:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 import requests
 from flask_cors import CORS
+from flask import request, jsonify
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def hello_world():
-    url = request.args.get('url').url
-    return url
+    url = request.args.get('url')
+    url1 = requests.get(url).url
+    return url1
 
 if __name__ == '__main__':
     app.run()
