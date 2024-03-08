0ad439a29caaaf21d544de386534a6795940d801
srbism2 <162742149+srbism2@users.noreply.github.com>
2024-03-09 00:27:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import requests
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello_world():
+url = request.args.get('url').url
+    return url
+
+if __name__ == '__main__':
+    app.run()
