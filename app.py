143573e24d092f933171c26b50f65d85cdee42b2
srbism2 <162742149+srbism2@users.noreply.github.com>
2024-03-09 13:45:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 import requests
 from flask_cors import CORS
 from flask import request, jsonify
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 CORS(app)
 
@@ -10,6 +11,51 @@ def hello_world():
     url = request.args.get('url')
     url1 = requests.get(url).url
     return jsonify(url1)
+
+@app.route('/flipkart_info/')
+def flip_info():
+    url = request.args.get('url')
+    flipkart_response = hit_url(flipkart_url)
+    price = get_flipkart_price(flipkart_response)
+    image = get_flipkart_image(flipkart_response)
+    
+    # Display the fetched data
+    flipkart_json = {"image" : image, "price" : price}
+    return jsonify(flipkart_json)
+    
+def get_flipkart_price(response):
+    try:
+        response = response
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            price = soup.find('div', {'class': '_30jeq3 _16Jk6d'}).text.strip()
+            return price
+        else:
+            print("Failed to retrieve price from Flipkart. Status code:", response.status_code)
+            return None
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return None
+
+def get_flipkart_image(response):
+    try:
+        response = response
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            image_tag = soup.find('img', {'class': '_396cs4 _2amPTt _3qGmMb'})
+            if image_tag:
+                image_url = image_tag['src']
+                return image_url
+            else:
+                print("Image not found on Flipkart.")
+                return None
+        else:
+            print("Failed to retrieve image from Flipkart. Status code:", response.status_code)
+            return None
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return None    
+
 
 if __name__ == '__main__':
     app.run()
