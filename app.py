10d99d64496d4b53067696d8e5e5fb5148c9e5f3
srbism2 <162742149+srbism2@users.noreply.github.com>
2024-03-09 14:48:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def get_flipkart_image(response):
 def hit_url(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url)
         print("response is ",response)
         if response.status_code == 200:
             return response
